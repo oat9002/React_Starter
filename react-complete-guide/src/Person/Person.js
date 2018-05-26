46a7e0c5ba9bae98b8b436991c78aea545e5584b
oat9002 <oat9002@hotmail.com>
2018-05-27 02:07:11 +0700

    add css

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Person.css'
 
 const person = (props) => {
     return (
-        <div onClick={ props.click }>
+        <div onClick={ props.click } className='Person'>
             <h1>I'm a {props.name}! I'm {props.age} years old.</h1>
             <p>{ props.children }</p>
             <input type='text' onChange={props.changed}/>
