7df8273388672a06229ad9937e303e2f7c0616c4
oat9002 <oat9002@hotmail.com>
2018-05-26 16:33:58 +0700

    Follow udemy react course

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,13 +3,53 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons : [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 27 },
+    ],
+
+  }
+
+  switchNameHandler = (name) => {
+    this.setState({
+      persons : [
+        { name: name, age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 26 },
+      ]
+    });
+  }
+
+  changeHandler = (event) => {
+    this.setState({
+      persons : [
+        { name: 'Max', age: 28 },
+        { name: event.target.value, age: 29 },
+        { name: 'Stephanie', age: 26 },
+      ]
+    });
+  }
+
   render() {
     return (
       <div className='App'>
         <h1>Hi, I'm and React app 555</h1>
         <p>I'm working</p>
-        <Person />
-        <Person />>
+        <button onClick={ () => this.switchNameHandler('oat') }>swtichName</button>
+        <Person 
+          name={ this.state.persons[0].name } 
+          age={ this.state.persons[0].age }/>
+        <Person 
+          name={ this.state.persons[1].name } 
+          age={ this.state.persons[1].age }
+          click={ this.switchNameHandler.bind(this, 'Irene') }
+          changed={ this.changeHandler }
+          >Hi!!!!!!</Person>
+        <Person 
+          name={ this.state.persons[2].name } 
+          age={ this.state.persons[2].age }/>
       </div>
     );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi'))
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
-const person = () => {
-    return <p>I'm a Person!</p>
+const person = (props) => {
+    return (
+        <div onClick={ props.click }>
+            <h1>I'm a {props.name}! I'm {props.age} years old.</h1>
+            <p>{ props.children }</p>
+            <input type='text' onChange={props.changed}/>
+        </div>
+    );
 };
 
 export default person;
