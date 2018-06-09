5131803e10d39def48c9ba2fa10b7dc1c67a24ad
Sarun Rakwijitsil <Sarun.Rakwijitsil@agoda.com>
2018-06-10 00:26:40 +0700

    Added errorboudary

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.css';
 import Person from './Person/Person';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
   state = {
@@ -59,12 +60,14 @@ class App extends Component {
       persons = (
         <div>
           { this.state.persons.map((person, index) => 
-              <Person 
-                name={ person.name } 
-                age={ person.age } 
-                click={ () => this.deletePersonHandler(index) } 
-                key={ person.id } 
-                changed={ (event) => this.nameChangedHandler(event, person.id) }/>
+              <ErrorBoundary key={ person.id }>
+                 <Person 
+                  name={ person.name } 
+                  age={ person.age } 
+                  //click={ () => this.deletePersonHandler(index) } 
+                  key={ person.id } 
+                  changed={ (event) => this.nameChangedHandler(event, person.id) }/>
+              </ErrorBoundary>
           ) }
       </div> 
       );
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -2,12 +2,14 @@ import React from 'react';
 import classes from './Person.css';
 
 const person = (props) => {
-    const style = {
-       
-    };
+    const rnd = Math.random();
+
+    if(rnd > 0.7) {
+        throw new Error('Something went wrong');
+    }
 
     return (
-        <div onClick={ props.click } className={ classes.Person } style={ style }>
+        <div onClick={ props.click } className={ classes.Person } >
             <h1>I'm a {props.name}! I'm {props.age} years old.</h1>
             <p>{ props.children }</p>
             <input type='text' onChange={props.changed}/>
