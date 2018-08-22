50bd15fa76a62ffc406bd579643b7d45a04f9d1a
oat9002 <oat9002@hotmail.com>
2018-08-23 01:21:41 +0700

    Added modal

--- burger/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -91,6 +91,22 @@ class BurgerBuilder extends Component {
         });
     }
 
+    purchaseCancelHandler = () => {
+        this.setState({
+            purchasing: false
+        });
+    }
+
+    purchaseContinueHandler = () => {
+        alert('You continue!');
+    }
+
+    purchaseCancelHandler = () => {
+        this.setState({
+            purchasing: false
+        });
+    }
+
     render() {
         const disabledInfo = {
             ...this.state.ingredients
@@ -102,8 +118,16 @@ class BurgerBuilder extends Component {
 
         return (
             <Aux>
-                <Modal show={ this.state.purchasing }>
-                    <OrderSummary ingredients={ this.state.ingredients }/> 
+                <Modal 
+                    show={ this.state.purchasing } 
+                    modalClosed={ this.purchaseCancelHandler }
+                >
+                    <OrderSummary 
+                        ingredients={ this.state.ingredients }
+                        purchaseCanceled={ this.purchaseCancelHandler }
+                        purchaseContinued={ this.purchaseContinueHandler }
+                        price={ this.state.totalPrice }
+                        /> 
                 </Modal>
                 <Burger ingredients={ this.state.ingredients }/>
                 <div>Build Controls</div>
