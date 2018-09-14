5aac742d257f9880e9d87079e010a6d9d9010621
oat9002 <oat9002@hotmail.com>
2018-09-15 01:20:33 +0700

    Added nav

--- burger/src/components/Navigation/Toolbar/Toolbar.js
+++ burger/src/components/Navigation/Toolbar/Toolbar.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import Logo from '../../Logo/Logo';
 import classes from './Toolbar.css';
+import NavigatoinItems from '../NamigationItems/NavigationItems';
 
 const toolbar = (props) => (
     <header className={ classes.Toolbar }>
         <div>MENU</div>
         <Logo />
-        <nav>
-            ...
-        </nav>
+        <NavigatoinItems />
     </header>
 );
 
