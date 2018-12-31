e2dab7ef30fc3c03f1fd843d48796f9b9f62abee
oat9002 <oat9002@hotmail.com>
2018-12-31 12:13:23 +0700

    Prepare react router

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -65,12 +65,7 @@ class Blog extends Component {
                 <section className="Posts">
                     { posts }
                 </section>
-                <section>
-                    <FullPost id={ this.state.selectedPostId } />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+                
             </div>
         );
     }
